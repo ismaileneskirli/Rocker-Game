b6f1f3a06fc58e91e8b9c728a02eaf8580141e0d
ismaileneskirli <69651222+ismaileneskirli@users.noreply.github.com>
2021-03-04 12:47:01 +0300

    rocket movement
    Rocket can now move

--- RocketGame/Assets/Scripts/Movement.cs
+++ RocketGame/Assets/Scripts/Movement.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationThrust = 100f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,18 +25,31 @@ public class Movement : MonoBehaviour
     void ProcessThrust()
     {
         if(Input.GetKey(KeyCode.Space)){
-            Debug.Log("Pressed Space");
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            //Debug.Log("Space is pressed");
         }
     }
 
     void ProcessRotation(){
 
-        if(Input.GetKey(KeyCode.A)){
-            Debug.Log("Rotating Left");
+        if(Input.GetKey(KeyCode.A))
+        {
+            //Debug.Log("Rotating Left");
+            Rotate(rotationThrust); // 0,0,1 -> change z axis.
         }
         // else cond used because users shouldn't press a and d at the same time. Cant go right and left at the sime time right :)
         else if(Input.GetKey(KeyCode.D)){
-            Debug.Log("Rotating Right");
+            //Debug.Log("Rotating Right");
+            Rotate(-rotationThrust); // 0,0,-1 -z axis
         }
+    }
+
+
+
+    // ctrl + . over copied line of code to make a new method.
+    // method for rotating the rocket.
+    private void Rotate(float rotationDirection)
+    {
+        transform.Rotate(Vector3.forward * rotationDirection * Time.deltaTime);
     }
 }
